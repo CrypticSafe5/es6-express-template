13f5028d9db25b4dd7113d27f5346474c0d87e94
CrypticSafe5 <WesleyBrackett94@gmail.com>
2020-02-14 22:38:34 -0600

    Added in new route and changed structure to be more conducive to testing

--- src/bin/routes.js
+++ src/bin/routes.js
@@ -1,10 +1,22 @@
 import express from 'express';
-import V1 from './middleware/V1';
+import V1 from './controllers/V1';
 
 const v1 = new V1();
 const router = express.Router();
 
-router.get('/V1/ping', v1.getPing);
-router.post('/V1/ping', v1.postPing);
+export function controllerWrapper(middleware, request, response) {
+    middleware(request).then((happyPath) => {
+        response.status(happyPath.status).send(happyPath.payload);
+    }).catch((notHappyPath) => {
+        response.status(notHappyPath.status).send(notHappyPath.payload);
+    });
+}
+
+// V1 routes go here
+router.get('/V1/ping', (req, res) => controllerWrapper(v1.getPing, req, res));
+router.post('/V1/ping', (req, res) => controllerWrapper(v1.postPing, req, res));
+router.get('/V1/googleLogo', (req, res) => controllerWrapper(v1.fetchGoogleLogo, req, res));
+
+// V2 and so forth keep getting tacked on below
 
 export default router;
